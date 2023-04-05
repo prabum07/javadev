1916980700319b84591d323507b06b851ca19987
prabu <prabum07@gmail.com>
2023-04-05 21:33:30 +0530

    to check @autowired in spring container object is same or not

--- src/main/java/com/prabu/loosecouple/PrinterController.java
+++ src/main/java/com/prabu/loosecouple/PrinterController.java
@@ -16,6 +16,10 @@ public class PrinterController {
 	Repo print;
 	
 	@Autowired
+	@Qualifier("newPrinter")
+	Repo print1;
+	
+	@Autowired
 	@Qualifier("oldPrinter")
 	Repo oldprint;
 	
@@ -23,6 +27,18 @@ public class PrinterController {
 	@ResponseBody
 	public String printme()
 	{
+		//-- these code to check whether spring container created object is actually singleton
+		
+		if(print.hashCode()==print1.hashCode())
+		{
+			System.out.print("same object");
+		}else
+		{
+			System.out.print("different object");
+
+		}
+		//-----
+		
 		System.out.print(oldprint.print());
 		return print.print();
 	}
