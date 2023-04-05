1c975bc9dedd3ac873d5b24e0755f0909e020def
prabu <prabum07@gmail.com>
2023-04-05 16:21:31 +0530

    singleton break with reflector feature
    singleton feature rule can be break by get that constructor of the class and setaccessible to true and with that constructor created object we can get the new instance.

--- src/main/java/com/prabu/FirstController.java
+++ src/main/java/com/prabu/FirstController.java
@@ -1,5 +1,8 @@
 package com.prabu;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import com.prabu.loosecouple.*;
@@ -18,10 +21,18 @@ public class FirstController {
 	
 	@RequestMapping("singleton")
 	@ResponseBody
-	public String singleton()
+	public String singleton() throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException
 	{
-		Singleton s=new Singleton();
-		s.st.print();
+		Singleton s= Singleton.getInstance();
+		System.out.println(s.hashCode());
+		
+		//still we can create two object of singleton class by below method.
+		Constructor<?> const1=(Constructor<?>) s.getClass().getDeclaredConstructor(new Class[0]);
+		const1.setAccessible(true);
+		Singleton s1=( Singleton)const1.newInstance();
+		System.out.print(s1.hashCode());
+
+
 		return "prabu";
 	}
 	
