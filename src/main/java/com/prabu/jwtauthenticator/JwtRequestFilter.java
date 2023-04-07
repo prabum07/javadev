e8030cdf11784ab6fefffb07ddf3d673ab9c5812
prabu <prabum07@gmail.com>
2023-04-07 11:20:19 +0530

    jwt spring security

---
+++ src/main/java/com/prabu/jwtauthenticator/JwtRequestFilter.java
@@ -0,0 +1,55 @@
+package com.prabu.jwtauthenticator;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+	@Autowired
+	private JwtUserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		final String requestTokenHeader=request.getHeader("Authorization");
+		String username=null;
+		String jwtToken =null;
+		
+		if(requestTokenHeader !=null && requestTokenHeader.startsWith("Bearer "))
+		{
+			jwtToken=requestTokenHeader.substring(7);
+			try {
+				username=jwtTokenUtil.getUsernameFromToken(jwtToken);	
+				System.out.println("user present");
+			}catch(IllegalArgumentException e)
+			{
+				System.out.println("unable to get token");
+			}catch (ExpiredJwtException e)
+			{
+				System.out.println("jwt token expired");
+
+			}
+		}else
+		{
+			
+		}
+		
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ src/main/java/com/prabu/jwtauthenticator/jwtauthenticatorController.java
@@ -0,0 +1,43 @@
+package com.prabu.jwtauthenticator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@RestController
+public class jwtauthenticatorController {
+
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@RequestMapping(value="/authenticate",method=RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticate(@RequestParam(value="username") String username,
+			@RequestParam(value="password") String password)
+	{
+		if(!username.equals("abcd") || !password.equals("1234"))
+		{
+			return ResponseEntity.ok("Invalid");
+
+		}
+		final UserDetails userDetails=userDetailsService.loadUserByUsername(username);
+		final String token = jwtTokenUtil.generateToken(userDetails);
+		return ResponseEntity.ok(token);
+	}
+	
+	@RequestMapping({"/hello"})
+	public String hello()
+	{
+		return "hello jwt";
+	}
+	
+}
