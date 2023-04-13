4ec273644d2230ea9c709e73069300f181fa6b08
prabu <prabum07@gmail.com>
2023-04-13 14:59:38 +0530

    loggers

--- src/main/java/com/prabu/configurationProperties/ConfigPropController.java
+++ src/main/java/com/prabu/configurationProperties/ConfigPropController.java
@@ -1,5 +1,7 @@
 package com.prabu.configurationProperties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +15,12 @@ public class ConfigPropController {
 	@Autowired
 	ApiConfig apiconfig;
 	
+	Logger logger=LoggerFactory.getLogger(ConfigPropController.class);
+	
 	@GetMapping("/configs")
 	public String hello()
 	{
+		logger.debug("logggin");
 		return "j";
 	}
 	@RequestMapping("configsss")
