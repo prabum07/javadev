a9fe5efafa487a751937df121f9a386052b471cc
prabu <prabum07@gmail.com>
2023-04-15 17:21:20 +0530

    functional interface

--- src/main/java/com/prabu/lamdaexpression/lambaDemo.java
+++ src/main/java/com/prabu/lamdaexpression/lambaDemo.java
@@ -7,6 +7,7 @@ import java.util.function.Predicate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class lambaDemo {
 	List<Integer> s1=new ArrayList<Integer>();
@@ -27,14 +28,34 @@ public class lambaDemo {
 
 
 	//System.out.print(	lb.temp(f->false));
+		Predicate<Integer> pred=(a) -> a==1;
+
 	for( int i=0;i<lb.s1.size();i++)
 	{
 		int it=lb.s1.get(i);
 		//int it=2;
-		Predicate<Integer> pred=(a) -> a==1;
-		System.out.print(	pred.test(it));
+	//	System.out.print(	pred.test(it));
 
 	}
+	List<student1> allStudents=new ArrayList<student1>();
+	student1 sd1=new student1();
+	sd1.setName("prabu");
+	sd1.setStd(1);
+	student1 sd2=new student1();
+	sd2.setName("raj");
+	sd2.setStd(2);
+	allStudents.add(sd1);
+	allStudents.add(sd2);
+	//implementing the predicate interface
+	Predicate<Integer> pred1=(temp)->temp.equals(1);
+	for(int i=0;i<allStudents.size();i++)
+	{
+		
+		System.out.print(	pred.test(allStudents.get(i).getStd()));
+
+	}
+
+	
 	}
 	
 	public  boolean temp(Predicate<Integer> ss)
@@ -57,4 +78,21 @@ public class lambaDemo {
 		s.forEach((temp) -> System.out.print(temp));
 		return "lamba";
 	}
+}
+ class student1{
+	 public String name;
+	 public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getStd() {
+		return std;
+	}
+	public void setStd(int std) {
+		this.std = std;
+	}
+	public int std;
+	
 }
